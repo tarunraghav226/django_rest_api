a83a23aac73df249d472941389cf6a5780b51759
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-24 20:03:55 +0530

    Updated settings of project.

--- studentwebapp/studentwebapp/settings.py
+++ studentwebapp/studentwebapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student.apps.StudentConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
