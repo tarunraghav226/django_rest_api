a5be3031c8a1bb3aa46e28f24bd855b04917e96e
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-24 20:11:37 +0530

    Added student model.

--- studentwebapp/student/models.py
+++ studentwebapp/student/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=10)
+    school_name = models.CharField(max_length=10)
+    mother = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'Student'
+
+    def __str__(self):
+        return self.name
