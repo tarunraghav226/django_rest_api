8049418a0b259f59410ea02b5f0ac71043858f1c
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-25 14:06:55 +0530

    Updated settings and url.

--- studentwebapp/studentwebapp/urls.py
+++ studentwebapp/studentwebapp/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from student import views
 
+from django.conf.urls.static import static
+from . import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('students/', views.student_list)
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
